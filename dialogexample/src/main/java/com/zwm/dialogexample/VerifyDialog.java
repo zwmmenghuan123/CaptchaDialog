f9bd4dcafa78abcfa126ad383215f5c877c9a3bf
Max <zwmmenghuan123@163.com>
2017-07-24 17:21:07 +0800

    second v

--- dialogexample/src/main/java/com/zwm/dialogexample/VerifyDialog.java
+++ dialogexample/src/main/java/com/zwm/dialogexample/VerifyDialog.java
@@ -2,29 +2,37 @@ package com.zwm.dialogexample;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zwm on 2017/7/21.
  */
 
-public class VerifyDialog extends Dialog {
+public class VerifyDialog extends Dialog implements TextWatcher,View.OnKeyListener{
 
     private Context context;
     private VerifyCallBack callBack;
 
-    private EditText editText1;
-    private EditText editText2;
-    private EditText editText3;
-    private EditText editText4;
+    private EditText captcha1;
+    private EditText captcha2;
+    private EditText captcha3;
+    private EditText captcha4;
+    private int current = 0;
+    private List<EditText> editTexts;
+
+    ImageView iv_close;
 
 
     protected VerifyDialog(Context context, VerifyCallBack callBack) {
@@ -32,6 +40,8 @@ public class VerifyDialog extends Dialog {
         this.context = context;
         this.callBack = callBack;
         setCancelable(false);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         initView();
 
     }
@@ -40,95 +50,83 @@ public class VerifyDialog extends Dialog {
         super(context, cancelable, cancelListener);
     }
 
-    protected VerifyDialog(Context context, int themeResId) {
-        super(context, themeResId);
-    }
 
     private void initView() {
-        setContentView(R.layout.verifydialog);
-        editText1 = (EditText) findViewById(R.id.confirmation1);
-        editText2 = (EditText) findViewById(R.id.confirmation2);
-        editText3 = (EditText) findViewById(R.id.confirmation3);
-        editText4 = (EditText) findViewById(R.id.confirmation4);
-        focus(editText1);
-        editText1.addTextChangedListener(new TextWatcher() {
+        setContentView(R.layout.layout_captchadialog);
+        iv_close = (ImageView) findViewById(R.id.iv_close);
+        iv_close.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                focus(editText2);
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
+            public void onClick(View v) {
+                dismiss();
             }
         });
-        editText2.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        captcha1 = (EditText) findViewById(R.id.captcha1);
+        captcha2 = (EditText) findViewById(R.id.captcha2);
+        captcha3 = (EditText) findViewById(R.id.captcha3);
+        captcha4 = (EditText) findViewById(R.id.captcha4);
+        editTexts = new ArrayList<EditText>();
 
-            }
+        editTexts.add(captcha1);
+        editTexts.add(captcha2);
+        editTexts.add(captcha3);
+        editTexts.add(captcha4);
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                focus(editText3);
-            }
+        captcha1.addTextChangedListener(this);
+        captcha2.addTextChangedListener(this);
+        captcha3.addTextChangedListener(this);
+        captcha4.addTextChangedListener(this);
 
-            @Override
-            public void afterTextChanged(Editable s) {
+        captcha1.setOnKeyListener(this);
+        captcha2.setOnKeyListener(this);
+        captcha3.setOnKeyListener(this);
+        captcha4.setOnKeyListener(this);
 
-            }
-        });
-        editText3.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        focus(captcha1);
 
-            }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                focus(editText4);
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        editText4.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                String[] inputNum = {
-                        editText1.getText().toString(),
-                        editText2.getText().toString(),
-                        editText3.getText().toString(),
-                        editText4.getText().toString(),
-                };
-                if (!"".equals(s.toString())) {
-                    callBack.getInput(inputNum);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+    }
 
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if ("".equals(s.toString())) {
+            return;
+        }
+        if (current > 2) {
+            //回调Activity传来的接口进行校验
+            callBack.getInput(
+                    new String[] {
+                            captcha1.getText().toString(),
+                            captcha2.getText().toString(),
+                            captcha3.getText().toString(),
+                            captcha4.getText().toString(),
+                    }
+            );
+            return;
+        }
+        focus(editTexts.get(++current));
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_DEL && event.getAction() == KeyEvent.ACTION_DOWN) {
+            editTexts.get(--current).setText("");
+            focus(editTexts.get(current));
+            return true;
+        }else{
+
+        }
+        return false;
+    }
 
 
     public interface VerifyCallBack {
@@ -144,11 +142,12 @@ public class VerifyDialog extends Dialog {
 
     //校验失败清空数据
     public void clear() {
-        editText1.setText("");
-        editText2.setText("");
-        editText3.setText("");
-        editText4.setText("");
-        focus(editText1);
+        captcha1.setText("");
+        captcha2.setText("");
+        captcha3.setText("");
+        captcha4.setText("");
+        current = 0;
+        focus(captcha1);
     }
 
 
